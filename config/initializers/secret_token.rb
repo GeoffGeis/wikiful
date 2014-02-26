353f8537d19bd824f1fb42f57021f3d8c0e65a88
GeoffGeis <geoffgeis@hotmail.com>
2014-02-26 05:43:55 +0000

    updated config/routes.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikiful::Application.config.secret_key_base = '2322d4453d8db43b53ae67a7f0215b582d784ede78067f2c4e4a974fe3f12c3648fa98e57f39e8749596dd9799f19a92653ba04f1860fcdf0ab5a6d17953b328'
