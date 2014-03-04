289e9db4db94a0e7242ad1db61711c901c3419e6
GeoffGeis <geoffgeis@hotmail.com>
2014-03-04 04:56:50 +0000

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304041015) do
+ActiveRecord::Schema.define(version: 20140304043155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "article_categories", force: true do |t|
-    t.ineger "article_id"
-    t.integer "category_id" 
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -29,9 +27,9 @@ ActiveRecord::Schema.define(version: 20140304041015) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-  
+
   create_table "categories", force: true do |t|
-    t.string "name"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
