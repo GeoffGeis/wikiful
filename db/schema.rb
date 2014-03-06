f35a88ad38793fdc9310a668b5053f288bfa9c78
GeoffGeis <geoffgeis@hotmail.com>
2014-03-05 23:40:44 -0500

    YES

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304043155) do
+ActiveRecord::Schema.define(version: 20131113175723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "article_categories", force: true do |t|
+    t.integer  "article_id"
+    t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
