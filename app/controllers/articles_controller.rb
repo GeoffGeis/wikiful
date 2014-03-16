3bf85333f8d9ad759d35942a5c97a5ec1bc35c9e
GeoffGeis <geoffgeis@hotmail.com>
2014-03-16 00:50:43 -0500

    updated articles controller and worked on _form and new html views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,15 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render "new"
+    end
   end
 end
